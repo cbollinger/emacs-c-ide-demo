46370fa1087f449f480151fd8e60182e98fc272f
Christian Bollinger <christian.bollinger@hispeed.ch>
2020-03-17 15:50:29 +0100

    disable package-check-signature

--- init.el
+++ init.el
@@ -2,7 +2,8 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (require 'package)
-(add-to-list 'package-archives '("org" . "http://orgmode.org") t) ; Org-mode's repository
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
+(setq package-check-signature nil)
 
 (package-initialize)
 
